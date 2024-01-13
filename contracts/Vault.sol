eec9dcea2ee9904ae11dbbf2c5a40d58a1337789
simonbolt <150022963+simonbolt@users.noreply.github.com>
2024-01-13 20:47:11 +0800

    added initial redeem flow

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -1,24 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IERC20, ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
+import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
 import "@openzeppelin/contracts/utils/structs/DoubleEndedQueue.sol";
 
-// receiver reciever
-struct QueueItem {
+// track number of shares and number of assets?
+
+struct DepositItem {
     address sender; // ? Should we store this?
     address receiver;
     uint256 amount; // Assets for deposits, shares for withdrawals
     uint32 timestamp;
 }
 
-library VaultQueue {
+struct RedeemItem {
+    address caller;
+    address owner;
+    address receiver;
+    uint256 shares;
+    uint32 timestamp;
+}
+
+library DepositQueue {
     struct Queue {
         uint128 first;
         uint128 last;
-        mapping(uint128 => QueueItem) items;
+        mapping(uint128 => DepositItem) items;
     }
 
     function initialize(Queue storage queue) internal {
@@ -26,22 +37,59 @@ library VaultQueue {
         queue.last = 1;
     }
 
-    function pushBack(Queue storage queue, QueueItem memory item) internal {
+    function pushBack(Queue storage queue, DepositItem memory item) internal {
         queue.items[queue.last] = item;
         queue.last += 1;
     }
 
-    function popFront(Queue storage queue) internal returns (QueueItem memory) {
+    function popFront(
+        Queue storage queue
+    ) internal returns (DepositItem memory) {
         require(queue.last > queue.first, "Queue is empty");
-        QueueItem memory item = queue.items[queue.first];
+        DepositItem memory item = queue.items[queue.first];
         delete queue.items[queue.first];
         queue.first += 1;
         return item;
     }
 }
 
-contract Vault is ERC20, Ownable {
+library RedeemQueue {
+    struct Queue {
+        uint128 first;
+        uint128 last;
+        mapping(uint128 => RedeemItem) items;
+    }
+
+    function initialize(Queue storage queue) internal {
+        queue.first = 1;
+        queue.last = 1;
+    }
+
+    function pushBack(Queue storage queue, RedeemItem memory item) internal {
+        queue.items[queue.last] = item;
+        queue.last += 1;
+    }
+
+    function popFront(
+        Queue storage queue
+    ) internal returns (RedeemItem memory) {
+        require(queue.last > queue.first, "Queue is empty");
+        RedeemItem memory item = queue.items[queue.first];
+        delete queue.items[queue.first];
+        queue.first += 1;
+        return item;
+    }
+}
+
+// Remove from queue
+contract Vault is ERC20, Ownable, Pausable {
     using Math for uint256;
+    using SafeERC20 for IERC20;
+
+    /**
+     * @dev Attempted to redeem more shares than the max amount for `receiver`.
+     */
+    error ERC4626ExceededMaxRedeem(address owner, uint256 shares, uint256 max);
 
     event Deposit(
         address indexed sender,
@@ -64,8 +112,9 @@ contract Vault is ERC20, Ownable {
     uint256 public priceUpdatedAt;
     uint256 public withdrawalFee;
     uint256 public totalDepositedAssets;
-    uint256 public totalWithdrawnssets;
-    VaultQueue.Queue myQueue;
+    uint256 public totalWithdrawnAssets;
+    DepositQueue.Queue myQueue;
+    RedeemQueue.Queue withdrawalQueue;
 
     modifier onlyUpdatedPrice() {
         require(priceUpdatedAt > block.timestamp - 1 days, "Price is outdated");
@@ -90,7 +139,7 @@ contract Vault is ERC20, Ownable {
     }
 
     function totalAssets() public view virtual returns (uint256) {
-        return totalDepositedAssets - totalWithdrawnssets;
+        return totalDepositedAssets - totalWithdrawnAssets;
     }
 
     function updatePrice(uint256 newPrice) public onlyOwner {
@@ -102,8 +151,8 @@ contract Vault is ERC20, Ownable {
         withdrawalFee = withdrawalFee_;
     }
 
-    function depositQueue() public view returns (QueueItem[] memory) {
-        QueueItem[] memory queueItems = new QueueItem[](
+    function depositQueue() public view returns (DepositItem[] memory) {
+        DepositItem[] memory queueItems = new DepositItem[](
             myQueue.last - myQueue.first
         );
         for (uint128 i = myQueue.first; i < myQueue.last; i++) {
@@ -112,17 +161,30 @@ contract Vault is ERC20, Ownable {
         return queueItems;
     }
 
+    function redeemQueue() public view returns (RedeemItem[] memory) {
+        RedeemItem[] memory queueItems = new RedeemItem[](
+            withdrawalQueue.last - withdrawalQueue.first
+        );
+        for (uint128 i = withdrawalQueue.first; i < withdrawalQueue.last; i++) {
+            queueItems[i - withdrawalQueue.first] = withdrawalQueue.items[i];
+        }
+        return queueItems;
+    }
+
     // ? Should we rename this to something else since its not strictly a deposit
-    function deposit(uint256 assets, address receiver) public virtual {
-        _asset.transferFrom(_msgSender(), address(this), assets);
-        _asset.transfer(owner(), assets);
-        QueueItem memory item = QueueItem(
+    function deposit(
+        uint256 assets,
+        address receiver
+    ) public virtual whenNotPaused {
+        _asset.safeTransferFrom(_msgSender(), address(this), assets);
+        _asset.safeTransfer(owner(), assets);
+        DepositItem memory item = DepositItem(
             msg.sender,
             receiver,
             assets,
             uint32(block.timestamp)
         );
-        VaultQueue.pushBack(myQueue, item);
+        DepositQueue.pushBack(myQueue, item);
     }
 
     function convertToShares(
@@ -139,11 +201,79 @@ contract Vault is ERC20, Ownable {
 
     function processDeposits(uint128 number) public onlyOwner onlyUpdatedPrice {
         for (uint128 i = 0; i < number; i++) {
-            QueueItem memory item = VaultQueue.popFront(myQueue);
+            DepositItem memory item = DepositQueue.popFront(myQueue);
             uint256 shares = convertToAssets(item.amount);
             _mint(item.receiver, shares);
             totalDepositedAssets += item.amount;
             emit Deposit(item.sender, item.receiver, item.amount, shares);
         }
+    }
+
+    /** @dev See {IERC4626-maxRedeem}. */
+    // Owner is the owner of the shares 4626 terminalogy
+    function maxRedeem(address owner) public view virtual returns (uint256) {
+        return balanceOf(owner);
+    }
+
+    function redeem(
+        uint256 shares,
+        address receiver,
+        address owner
+    ) public virtual whenNotPaused {
+        uint256 maxShares = maxRedeem(owner);
+        if (shares > maxShares) {
+            revert ERC4626ExceededMaxRedeem(owner, shares, maxShares);
+        }
+        // Transfer to contract first,
+        _transfer(_msgSender(), address(this), shares);
+
+        // Add to queue
+        RedeemItem memory item = RedeemItem(
+            _msgSender(),
+            owner,
+            receiver,
+            shares,
+            uint32(block.timestamp)
+        );
+        RedeemQueue.pushBack(withdrawalQueue, item);
+    }
+
+    function processRedeems(
+        uint128 number,
+        uint256 amount
+    ) public onlyOwner onlyUpdatedPrice {
+        _asset.safeTransferFrom(owner(), address(this), amount);
+
+        for (uint128 i = 0; i < number; i++) {
+            RedeemItem memory item = RedeemQueue.popFront(withdrawalQueue);
+            uint256 assets = convertToShares(item.shares);
+            _burn(address(this), item.shares);
+            _asset.safeTransfer(item.receiver, assets);
+            totalWithdrawnAssets += assets; // ? # shares and # assets not in sync
+            emit Withdraw(
+                item.caller,
+                item.receiver,
+                item.owner,
+                assets,
+                item.shares
+            );
+        }
+
+        require(_asset.balanceOf(address(this)) == 0, "Incorrect amount given");
+    }
+
+    /// @dev ETH can't be recieve as the contract has no fallback function
+    function withdrawalToOwner(IERC20 token) external onlyOwner {
+        uint256 balance = token.balanceOf(address(this));
+        require(balance > 0, "Contract has no balance");
+        require(token.transfer(owner(), balance), "Transfer failed");
+    }
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
     }
 }
--- test/Vault.test.ts
+++ test/Vault.test.ts
@@ -4,13 +4,27 @@ import {
 } from "@nomicfoundation/hardhat-toolbox-viem/network-helpers";
 import chai, { expect } from "chai";
 import chaiAsPromised from "chai-as-promised";
-import hre, { getNamedAccounts } from "hardhat";
-import { get } from "http";
-import { getAddress, parseGwei, parseEven, decodeEventLog } from "viem";
+import hre from "hardhat";
+import { decodeEventLog, getAddress } from "viem";
 
 chai.use(chaiAsPromised);
 describe("Vault", function () {
-  async function deployVaultFixture() {
+  async function getEmittedEvent(
+    hash: `0x${string}`,
+    abi: any,
+    eventName: string,
+  ) {
+    const publicClient = await hre.viem.getPublicClient();
+    const { logs } = await publicClient.getTransactionReceipt({ hash });
+    for (let log of logs) {
+      const event = decodeEventLog({ abi, data: log.data, topics: log.topics });
+      if (event.eventName === eventName) {
+        return event.args;
+      }
+    }
+    return {};
+  }
+  async function fixtureNewVault() {
     const [deployer, owner, user] = await hre.viem.getWalletClients();
 
     // await hre.deployments.fixture(['Vault']);
@@ -39,27 +53,78 @@ describe("Vault", function () {
       owner: owner,
     };
   }
+
+  async function fixtureWithPendingDeposit() {
+    const deployment = await loadFixture(fixtureNewVault);
+
+    const { vault, user, usdt } = await loadFixture(fixtureNewVault);
+
+    await usdt.write.approve([vault.address, toBN(10_000, 6)], {
+      account: user.account,
+    });
+
+    await vault.write.deposit([toBN(10_000, 6), user.account.address], {
+      account: user.account,
+    });
+
+    return deployment;
+  }
+
+  async function fixtureWithDeposit() {
+    const deployment = await loadFixture(fixtureWithPendingDeposit);
+
+    const { vault, owner } = deployment;
+
+    await vault.write.updatePrice([BigInt(10 ** (18 - 6))], {
+      account: owner.account,
+    });
+
+    expect(await vault.read.totalSupply()).to.equal(BigInt(0));
+    expect(await vault.read.totalAssets()).to.equal(BigInt(0));
+
+    await vault.write.processDeposits([BigInt(1)], { account: owner.account });
+
+    return deployment;
+  }
+
+  async function fixtureWithPendingRedeem() {
+    const deployment = await loadFixture(fixtureWithDeposit);
+    const { vault, user } = deployment;
+
+    await vault.write.redeem(
+      [toBN(10_000, 18), user.account.address, user.account.address],
+      {
+        account: user.account,
+      },
+    );
+
+    return deployment;
+  }
+
   describe("Deployment", function () {
     it("Should set the right name and symbol", async function () {
-      const { vault } = await loadFixture(deployVaultFixture);
+      const { vault } = await loadFixture(fixtureNewVault);
       expect(await vault.read.name()).to.equal("Vault");
       expect(await vault.read.symbol()).to.equal("BLT");
     });
 
-    it("Should set the right asset");
+    it("Should set the right asset", async function () {
+      const { vault, usdt } = await loadFixture(fixtureNewVault);
+      expect(await vault.read.asset()).to.equal(getAddress(usdt.address));
+    });
 
     it("Sets the correct initial price", async function () {
-      const { vault } = await loadFixture(deployVaultFixture);
+      const { vault } = await loadFixture(fixtureNewVault);
       expect(await vault.read.price()).to.equal(BigInt(0));
       expect(await vault.read.priceUpdatedAt()).to.equal(BigInt(0));
     });
     it("Sets the correct initial withdrawal fee", async function () {
-      const { vault } = await loadFixture(deployVaultFixture);
+      const { vault } = await loadFixture(fixtureNewVault);
       expect(await vault.read.withdrawalFee()).to.equal(BigInt(0));
     });
 
     it("Should set the right owner", async function () {
-      const { vault, owner } = await loadFixture(deployVaultFixture);
+      const { vault, owner } = await loadFixture(fixtureNewVault);
 
       expect(await vault.read.owner()).to.equal(
         getAddress(owner.account.address),
@@ -67,9 +132,74 @@ describe("Vault", function () {
     });
   });
 
+  describe("Ownership", function () {
+    it("Can change owner", async function () {
+      const { vault, owner, user } = await loadFixture(fixtureNewVault);
+      await vault.write.transferOwnership([user.account.address], {
+        account: owner.account,
+      });
+
+      expect(await vault.read.owner()).to.equal(
+        getAddress(user.account.address),
+      );
+    });
+
+    it("Can reject changing to null address", async function () {
+      const { vault, owner } = await loadFixture(fixtureNewVault);
+      await expect(
+        vault.write.transferOwnership(
+          ["0x0000000000000000000000000000000000000000"],
+          {
+            account: owner.account,
+          },
+        ),
+      ).to.eventually.be.rejectedWith(
+        'OwnableInvalidOwner("0x0000000000000000000000000000000000000000")',
+      );
+    });
+  });
+  describe("Pauseable", function () {
+    it("Rejects being paused paused by user", async function () {
+      const { vault, user } = await loadFixture(fixtureNewVault);
+      expect(await vault.read.paused()).to.equal(false);
+      await expect(
+        vault.write.pause({ account: user.account }),
+      ).to.eventually.be.rejectedWith("OwnableUnauthorizedAccount");
+    });
+
+    it("Can be paused by owner", async function () {
+      const { vault, owner, user } = await loadFixture(fixtureNewVault);
+      expect(await vault.read.paused()).to.equal(false);
+      await vault.write.pause({ account: owner.account });
+      expect(await vault.read.paused()).to.equal(true);
+    });
+    it("Can rejects user actions when paused", async function () {
+      const { vault, owner, user } = await loadFixture(fixtureNewVault);
+      await vault.write.pause({ account: owner.account });
+
+      await expect(
+        vault.write.deposit(
+          [toBN(10_000, 6), getAddress(user.account.address)],
+          { account: user.account },
+        ),
+      ).to.eventually.be.rejectedWith("EnforcedPause");
+
+      await expect(
+        vault.write.redeem(
+          [
+            toBN(10_000, 6),
+            getAddress(user.account.address),
+            getAddress(user.account.address),
+          ],
+          { account: user.account },
+        ),
+      ).to.eventually.be.rejectedWith("EnforcedPause");
+    });
+  });
+
   describe("Price management", function () {
     it("Owner can update price", async function () {
-      const { vault, owner } = await loadFixture(deployVaultFixture);
+      const { vault, owner } = await loadFixture(fixtureNewVault);
       const price = BigInt(10 ** 6);
       await vault.write.updatePrice([price], {
         account: owner.account,
@@ -81,7 +211,7 @@ describe("Vault", function () {
     });
 
     it("Deployer cannot update the price", async function () {
-      const { vault, deployer } = await loadFixture(deployVaultFixture);
+      const { vault, deployer } = await loadFixture(fixtureNewVault);
       await expect(
         vault.write.updatePrice([BigInt(10 ** 6)], {
           account: deployer.account,
@@ -90,7 +220,7 @@ describe("Vault", function () {
     });
 
     it("Users cannot update the price", async function () {
-      const { vault, user } = await loadFixture(deployVaultFixture);
+      const { vault, user } = await loadFixture(fixtureNewVault);
       await expect(
         vault.write.updatePrice([BigInt(10 ** 6)], {
           account: user.account,
@@ -101,7 +231,7 @@ describe("Vault", function () {
 
   describe("Withdrawal Fee management", function () {
     it("Owner can update withdrawal fee", async function () {
-      const { vault, owner } = await loadFixture(deployVaultFixture);
+      const { vault, owner } = await loadFixture(fixtureNewVault);
       const withdrawalFee = BigInt(10 ** 6);
       await vault.write.updateWithdrawalFee([withdrawalFee], {
         account: owner.account,
@@ -110,7 +240,7 @@ describe("Vault", function () {
     });
 
     it("Deployer cannot update the withdrawal fee", async function () {
-      const { vault, deployer } = await loadFixture(deployVaultFixture);
+      const { vault, deployer } = await loadFixture(fixtureNewVault);
       await expect(
         vault.write.updateWithdrawalFee([BigInt(10 ** 6)], {
           account: deployer.account,
@@ -119,7 +249,7 @@ describe("Vault", function () {
     });
 
     it("Users cannot update the withdrawal fee", async function () {
-      const { vault, user } = await loadFixture(deployVaultFixture);
+      const { vault, user } = await loadFixture(fixtureNewVault);
       await expect(
         vault.write.updateWithdrawalFee([BigInt(10 ** 6)], {
           account: user.account,
@@ -130,37 +260,36 @@ describe("Vault", function () {
 
   const toBN = (n: number, decimals = 6) => BigInt(n) * BigInt(10 ** decimals);
 
-  describe("Queue Deposit", function () {
+  describe("Deposit", function () {
     it("Can queue user deposit", async function () {
-      const { vault, usdt, user, owner } =
-        await loadFixture(deployVaultFixture);
+      const { vault, usdt, user, owner } = await loadFixture(fixtureNewVault);
 
       expect(await usdt.read.balanceOf([user.account.address])).to.equal(
-        toBN(100_000),
+        toBN(100_000, 6),
       );
 
-      await usdt.write.approve([vault.address, toBN(10_000)], {
+      await usdt.write.approve([vault.address, toBN(10_000, 6)], {
         account: user.account,
       });
 
-      await vault.write.deposit([toBN(10_000), user.account.address], {
+      await vault.write.deposit([toBN(10_000, 6), user.account.address], {
         account: user.account,
       });
 
       expect(await usdt.read.balanceOf([user.account.address])).to.equal(
-        toBN(90_000),
+        toBN(90_000, 6),
       );
 
-      expect(await usdt.read.balanceOf([vault.address])).to.equal(toBN(0));
+      expect(await usdt.read.balanceOf([vault.address])).to.equal(toBN(0, 6));
       expect(await usdt.read.balanceOf([owner.account.address])).to.equal(
-        toBN(10_000),
+        toBN(10_000, 6),
       );
 
       expect(await vault.read.depositQueue()).to.deep.equal([
         {
           sender: getAddress(user.account.address),
           receiver: getAddress(user.account.address),
-          amount: toBN(10_000),
+          amount: toBN(10_000, 6),
           timestamp: await time.latest(),
         },
       ]);
@@ -168,24 +297,8 @@ describe("Vault", function () {
   });
 
   describe("Process Deposit", function () {
-    async function depositedVaultFixture() {
-      const deployment = await loadFixture(deployVaultFixture);
-
-      const { vault, user, usdt } = await loadFixture(deployVaultFixture);
-
-      await usdt.write.approve([vault.address, toBN(10_000)], {
-        account: user.account,
-      });
-
-      await vault.write.deposit([toBN(10_000), user.account.address], {
-        account: user.account,
-      });
-
-      return deployment;
-    }
-
     it("Can reject when price is outdated", async function () {
-      const { vault, owner } = await loadFixture(depositedVaultFixture);
+      const { vault, owner } = await loadFixture(fixtureWithPendingDeposit);
       await expect(
         vault.write.processDeposits([BigInt(1)], {
           account: owner.account,
@@ -194,7 +307,9 @@ describe("Vault", function () {
     });
 
     it("Can reject when user ", async function () {
-      const { vault, owner, user } = await loadFixture(depositedVaultFixture);
+      const { vault, owner, user } = await loadFixture(
+        fixtureWithPendingDeposit,
+      );
       await vault.write.updatePrice([BigInt(10 ** (18 - 6))], {
         account: owner.account,
       });
@@ -205,22 +320,9 @@ describe("Vault", function () {
       ).to.eventually.be.rejectedWith("OwnableUnauthorizedAccount");
     });
 
-    async function getEmittedEvent(hash: `0x${string}`, abi: any, eventName: string) {
-      const publicClient = await hre.viem.getPublicClient();
-      const { logs } = await publicClient.getTransactionReceipt({ hash });
-      for (let log of logs) {
-
-        const event = decodeEventLog({ abi, data: log.data, topics: log.topics })
-        if (event.eventName === eventName) {
-          return event.args
-        }
-      }
-      return {}
-    }
-
     it("Can process queued deposits", async function () {
       const { vault, usdt, user, owner } = await loadFixture(
-        depositedVaultFixture,
+        fixtureWithPendingDeposit,
       );
 
       expect(await vault.read.depositQueue()).to.deep.equal([
@@ -238,18 +340,14 @@ describe("Vault", function () {
       expect(await vault.read.totalSupply()).to.equal(BigInt(0));
       expect(await vault.read.totalAssets()).to.equal(BigInt(0));
 
-      const hash = await vault.write.processDeposits([BigInt(1)],
-        { account: owner.account });
-
+      const hash = await vault.write.processDeposits([BigInt(1)], {
+        account: owner.account,
+      });
 
       expect(await vault.read.depositQueue()).to.deep.equal([]);
 
-      expect(await vault.read.totalAssets()).to.equal(
-        toBN(10_000, 6),
-      );
-      expect(await vault.read.totalSupply()).to.equal(
-        toBN(10_000, 18),
-      );
+      expect(await vault.read.totalAssets()).to.equal(toBN(10_000, 6));
+      expect(await vault.read.totalSupply()).to.equal(toBN(10_000, 18));
 
       expect(await vault.read.balanceOf([user.account.address])).to.equal(
         toBN(10_000, 18),
@@ -260,7 +358,142 @@ describe("Vault", function () {
         owner: getAddress(user.account.address),
         assets: toBN(10_000, 6),
         shares: toBN(10_000, 18),
-      })
+      });
+    });
+  });
+
+  describe("Redeem", function () {
+    it("Can queue user redeem", async function () {
+      const { vault, usdt, user } = await loadFixture(fixtureWithDeposit);
+
+      await expect(
+        usdt.read.balanceOf([user.account.address]),
+      ).to.eventually.equal(toBN(90_000, 6));
+      expect(await vault.read.balanceOf([user.account.address])).to.equal(
+        toBN(10_000, 18),
+      );
+      expect(await vault.read.totalSupply()).to.equal(toBN(10_000, 18));
+
+      await vault.write.redeem(
+        [toBN(10_000, 18), user.account.address, user.account.address],
+        {
+          account: user.account,
+        },
+      );
+
+      // Expect LP tokens to be transfered to contract, but no change in usdt balance
+      expect(await usdt.read.balanceOf([user.account.address])).to.equal(
+        toBN(90_000, 6),
+      );
+      expect(await vault.read.balanceOf([user.account.address])).to.equal(
+        toBN(0, 18),
+      );
+      expect(await vault.read.balanceOf([vault.address])).to.equal(
+        toBN(10_000, 18),
+      );
+      expect(await vault.read.totalSupply()).to.equal(toBN(10_000, 18));
+
+      expect(await vault.read.redeemQueue()).to.deep.equal([
+        {
+          caller: getAddress(user.account.address),
+          owner: getAddress(user.account.address),
+          receiver: getAddress(user.account.address),
+          shares: toBN(10_000, 18),
+          timestamp: await time.latest(),
+        },
+      ]);
+    });
+
+    it("Can process redeem", async () => {
+      const { vault, usdt, user, owner } = await loadFixture(
+        fixtureWithPendingRedeem,
+      );
+
+      expect(await usdt.read.balanceOf([owner.account.address])).to.equal(
+        toBN(10_000, 6),
+      );
+      expect(await usdt.read.balanceOf([user.account.address])).to.equal(
+        toBN(90_000, 6),
+      );
+      expect(await vault.read.redeemQueue()).to.deep.equal([
+        {
+          caller: getAddress(user.account.address),
+          owner: getAddress(user.account.address),
+          receiver: getAddress(user.account.address),
+          shares: toBN(10_000, 18),
+          timestamp: await time.latest(),
+        },
+      ]);
+      expect(await vault.read.totalAssets()).to.equal(toBN(10_000, 6));
+      expect(await vault.read.totalSupply()).to.equal(toBN(10_000, 18));
+
+      await usdt.write.approve([vault.address, toBN(10_000, 6)], {
+        account: owner.account,
+      });
+      const hash = await vault.write.processRedeems(
+        [BigInt(1), toBN(10_000, 6)],
+        { account: owner.account },
+      );
+
+      expect(await vault.read.redeemQueue()).to.deep.equal([]);
+
+      expect(await vault.read.totalAssets()).to.equal(toBN(0, 6));
+      expect(await vault.read.totalSupply()).to.equal(toBN(0, 18));
+
+      expect(await usdt.read.balanceOf([user.account.address])).to.equal(
+        toBN(100_000, 6),
+      );
+
+      expect(await getEmittedEvent(hash, vault.abi, "Withdraw")).to.deep.equal({
+        sender: getAddress(user.account.address),
+        receiver: getAddress(user.account.address),
+        owner: getAddress(user.account.address),
+        assets: toBN(10_000, 6),
+        shares: toBN(10_000, 18),
+      });
+    });
+  });
+
+  describe("Vault withdrawalToOwner", function () {
+    it("Can reject ETH transfers", async function () {
+      const { vault, user } = await loadFixture(fixtureNewVault);
+
+      await expect(
+        user.sendTransaction({
+          to: vault.address,
+          value: toBN(1, 18),
+        }),
+      ).to.eventually.be.rejectedWith(
+        "Transaction reverted: function selector was not recognized and there's no fallback nor receive function",
+      );
+    });
+
+    it("Should allow withdrawal to owner for USDT", async function () {
+      const { vault, owner, usdt, user } = await loadFixture(fixtureNewVault);
+
+      expect(await usdt.read.balanceOf([owner.account.address])).to.equal(
+        BigInt(0),
+      );
+
+      // Send USDT to vault
+      await usdt.write.transfer([vault.address, toBN(5_000, 6)], {
+        account: user.account,
+      });
+
+      expect(await usdt.read.balanceOf([vault.address])).to.equal(
+        toBN(5_000, 6),
+      );
+      expect(await vault.read.totalAssets()).to.equal(BigInt(0));
+
+      // Withdrawal to owner
+      await vault.write.withdrawalToOwner([usdt.address], {
+        account: owner.account,
+      });
+
+      expect(await usdt.read.balanceOf([vault.address])).to.equal(BigInt(0));
+      expect(await usdt.read.balanceOf([owner.account.address])).to.equal(
+        toBN(5_000, 6),
+      );
     });
   });
 });
