5e650b5b7efcf6da68183f6aa30630d149f4479d
simonbolt <150022963+simonbolt@users.noreply.github.com>
2024-01-22 21:34:12 +0800

    Change to deploy using openzeppelin defender (#1)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,25 @@
 import "@nomicfoundation/hardhat-toolbox-viem";
+import "@nomicfoundation/hardhat-verify";
+import "@openzeppelin/hardhat-upgrades";
 import "dotenv/config";
 import "hardhat-deploy";
-import { HardhatUserConfig } from "hardhat/config";
 import "solidity-docgen";
 
 const config = {
-  solidity: "0.8.20",
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  defender: {
+    apiKey: process.env.API_KEY,
+    apiSecret: process.env.API_SECRET,
+    useDefenderDeploy: true,
+  },
   docgen: {
     pages: "files",
     exclude: [],
@@ -16,10 +30,12 @@ const config = {
     },
     usdt: {
       arbitrumSepolia: "0xe9318560fF9B5Df59A669A03895effFacF0df8cb",
+      sepolia: "0xaa8e23fb1079ea71e0a56f48a2aa51851d8433d0",
     },
     owner: {
       default: 1,
       arbitrumSepolia: `${process.env.OWNER_PUBLIC_KEY}`,
+      sepolia: "0x86050D67dE08714AB8320469C4019bc1dd26bCF0",
     },
   },
   networks: {
@@ -35,6 +51,13 @@ const config = {
         },
       },
     },
+    sepolia: {
+      url: "https://ethereum-sepolia.publicnode.com",
+      chainId: 11155111,
+    },
+  },
+  etherscan: {
+    apiKey: `${process.env.ETHERSCAN_API_KEY}`,
   },
   verify: {
     etherscan: {
